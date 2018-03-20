265ea88cf8cdc6d569e37557d6392132ee94f091
Stephen Philp <swelltrain@gmail.com>
2018-03-19 17:15:28 -0700

    HomeHelper.formatted_tweet_text
      format the text of the tweet

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def formatted_tweet_text(text)
+    mentioned_user_regex = /(\@\w+)/
+    url_regex = /(https*:\/\/\S+)/
+    t = text.gsub(mentioned_user_regex) { |user| "#{link_to user, user_path(user)}" }
+    t.gsub!(url_regex) { |url| "#{link_to url, url}" }
+    t
+  end
 end
